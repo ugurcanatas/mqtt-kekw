7351d1d35d5a1d0abebd59e5478e6deea24be085
Ugurcan Emre Atas <22047299+ugurcanatas@users.noreply.github.com>
2021-11-08 15:09:34 +0300

    jestconf

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest/presets/default-esm", // or other ESM presets
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["src/test.ts"],
+};
+
+// "jest": {
+//   "testPathIgnorePatterns": [
+//     "src/test.ts"
+//   ]
+// },
